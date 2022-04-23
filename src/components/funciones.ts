54b3ab3c800b7bad0b0a39b1ee1517be3408546a
afrivera <andresrfelip@hotmail.com>
2022-04-22 21:29:42 -0500

    initial config webpack-typescript

---
+++ src/components/funciones.ts
@@ -0,0 +1,5 @@
+
+
+export const callBatman = (name: string): string =>{
+    return `Batman te llama ${name}`
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { callBatman } from "./components/funciones";
+
+
+
+console.log('Hola Mundo!');
+
+console.log( callBatman('Andres') );
